968db8430600fb0eca8ae0bafc738d1b2e0c82ae
uwilde <uriahwilde@gmail.com>
2025-09-24 22:32:45 -0500

    Add files via upload

---
+++ Explorerexe/explorer_data.js
@@ -0,0 +1,102 @@
+﻿window.EXPLORER_DATA = {
+    "type":  "folder",
+    "title":  "My Documents",
+    "children":  [
+                     {
+                         "name":  "Pictures",
+                         "type":  "auto_pictures",
+                         "children":  [
+                                          {
+                                              "name":  "Cards",
+                                              "type":  "folder",
+                                              "children":  [
+
+                                                           ]
+                                          }
+                                      ]
+                     },
+                     {
+                         "name":  "Videos",
+                         "type":  "folder",
+                         "children":  [
+                                          {
+                                              "name":  "Surprise.yt",
+                                              "type":  "youtube",
+                                              "url":  "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                                              "title":  "Surprise Video"
+                                          },
+                                          {
+                                              "name":  "YouCanRun.yt",
+                                              "type":  "youtube",
+                                              "url":  "https://youtu.be/8jF94CmDITrEJTRP9",
+                                              "title":  "You can run, but..."
+                                          },
+                                          {
+                                              "name":  "Big Buck Bunny.yt",
+                                              "type":  "youtube",
+                                              "url":  "https://youtu.be/aqz-KE-bpKQ",
+                                              "title":  "Demo"
+                                          }
+                                      ]
+                     },
+                     {
+                         "name":  "My PC",
+                         "type":  "folder",
+                         "children":  [
+                                          {
+                                              "name":  "Pictures",
+                                              "type":  "shortcut",
+                                              "to":  "/Pictures"
+                                          },
+                                          {
+                                              "name":  "Videos",
+                                              "type":  "shortcut",
+                                              "to":  "/Videos"
+                                          },
+                                          {
+                                              "name":  "Cards",
+                                              "type":  "shortcut",
+                                              "to":  "/Pictures/Cards"
+                                          },
+                                          {
+                                              "name":  "Local Disk (C:)",
+                                              "type":  "folder",
+                                              "icon":  "drive",
+                                              "details":  "120 GB free of 256 GB",
+                                              "children":  [
+                                                               {
+                                                                   "name":  "Program Files",
+                                                                   "type":  "folder",
+                                                                   "children":  [
+
+                                                                                ]
+                                                               },
+                                                               {
+                                                                   "name":  "Users",
+                                                                   "type":  "folder",
+                                                                   "children":  [
+
+                                                                                ]
+                                                               },
+                                                               {
+                                                                   "name":  "Windows",
+                                                                   "type":  "folder",
+                                                                   "children":  [
+
+                                                                                ]
+                                                               }
+                                                           ]
+                                          },
+                                          {
+                                              "name":  "CD Drive (D:)",
+                                              "type":  "folder",
+                                              "icon":  "disc",
+                                              "details":  "No disc inserted",
+                                              "children":  [
+
+                                                           ]
+                                          }
+                                      ]
+                     }
+                 ]
+};
